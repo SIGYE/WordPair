70efc6809d9fb39b2fb0e15bb1b95cbf458b81bc
Mwesigye Linda <lindamwesigye2@gmail.com>
2024-02-15 16:17:27 +0200

    updated List generation

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
         home: RandomWords());
@@ -19,16 +18,36 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  Widget _buildList(){
-    
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[500],
+          child: const Center(child: Text('Entry B')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[100],
+          child: const Center(child: Text('Entry C')),
+        ),
+      ],
+    );
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('WordPair Generator'),
-      ),
-      body: _buildList();
-    );
+        appBar: AppBar(
+          title: Text('WordPair Generator'),
+        ),
+        body: _buildList());
   }
 }
